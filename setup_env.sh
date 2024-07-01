7fce4ed154f693ee467292e8884ad7dac3a6a0cd
Ngo Hong Thai <ngohongthai.uet@gmail.com>
2024-07-02 00:17:21 +0700

    update code

---
+++ setup_env.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+pip install langchain
+pip install langchain-core
+pip install langchain-community
+pip install langchain-experimental
+pip install langgraph
+pip install "langserve[all]"
+pip install langchain-cli
+pip install crewai
+pip install 'crewai[tools]'
